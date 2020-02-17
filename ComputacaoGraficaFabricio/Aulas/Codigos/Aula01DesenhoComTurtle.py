d32571d08d2632a86c80f0b97389eea8004b9617
donijr <donizetigmjr@gmail.com>
2020-02-16 22:35:27 -0300

    Adicionando pasta Codigo

---
+++ ComputacaoGraficaFabricio/Aulas/Codigos/Aula01DesenhoComTurtle.py
@@ -0,0 +1,23 @@
+import turtle
+
+lapis = turtle.Pen()
+lapis.forward(100)
+lapis.right(90)
+lapis.forward(100)
+lapis.right(90)
+lapis.forward(100)
+lapis.right(90)
+lapis.forward(100)
+
+lapis.penup()
+lapis.forward(200)
+lapis.pendown()
+lapis.left(120)
+lapis.forward(100)
+lapis.right(120)
+lapis.forward(100)
+lapis.right(120)
+lapis.forward(100)
+
+input()
+
---
+++ ComputacaoGraficaFabricio/Aulas/Codigos/Aula01ExemplosDeSintaxe.py
@@ -0,0 +1,18 @@
+import os
+os.system("clear")
+
+x = 5
+
+print(type(x))
+
+x = "Fabricio"
+
+print(type(x))
+
+x = 3
+
+if x >= 10:
+    print(x >= 10)
+
+elif x < 5:
+        print(x <= 10)
