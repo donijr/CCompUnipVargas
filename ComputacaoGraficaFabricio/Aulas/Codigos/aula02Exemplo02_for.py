afb65777c5d03ce3946c23875d4433630170e2f8
donijr <donizetigmjr@gmail.com>
2020-02-22 21:15:31 -0300

    Adicionado comentários no código

--- ComputacaoGraficaFabricio/Aulas/Codigos/aula02Exemplo02_for.py
+++ ComputacaoGraficaFabricio/Aulas/Codigos/aula02Exemplo02_for.py
@@ -1,12 +1,17 @@
 import os
 
 os.system("clear")
-""" nome = "Fabricio" # forit
-
-for letra in nome:
+print("For em um String. String = Fabricio")
+nome = "Fabricio" 
+for letra in nome: # for percorre elementos na lista, no caso é uma string
     print(letra)
- """
 
- #for i in range(10) # for com limitação de passos, desse modo i = 0 e vai até n-1, com incremento de +1
+print("------------------------------------")
+print("Primeiro for")
+for i in range(10): # for com limitação de passos, desse modo i = 0 e vai até n-1, com incremento de +1
+    print (i)
+
+print("------------------------------------")
+print("Segundo for")
 for i in range(1,10,2): # for com limitacao de passos. for i in range (i=X, nº limite, incremento)
     print (i)
--- ComputacaoGraficaFabricio/Aulas/Codigos/aula02QuadradoComFor.py
+++ ComputacaoGraficaFabricio/Aulas/Codigos/aula02QuadradoComFor.py
@@ -1,12 +1,13 @@
 import turtle
 lapis = turtle.Pen()
 
-""" for i in range(4):
+""" for i in range(4):   # desnhando quadrado simples
     lapis.forward(100)
     lapis.right(90)
-input() """
+input() """ 
 
-""" for i in range(4):
+# Desenhando dois quadrados separados
+""" for i in range(4): 
     lapis.forward(100)
     lapis.right(90)
 
@@ -19,6 +20,7 @@ for i in range(4):
     lapis.right(90)
 input() """
 
+# Desenhando dois quadrados separados, cada um de uma cor
 lapis.color("Red") # colocando cor na caneta
 for i in range(4):
     lapis.forward(100)
